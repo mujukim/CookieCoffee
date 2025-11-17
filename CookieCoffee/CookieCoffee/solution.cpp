2fe53d65f537e15dd628f86b9f2dbb7a8557af51
Muju <anwn0414@naver.com>
2025-11-18 08:06:07 +0900

    일반 다익스트라 복기

--- CookieCoffee/CookieCoffee/solution.cpp
+++ CookieCoffee/CookieCoffee/solution.cpp
@@ -8,10 +8,11 @@
 // 1. 모든 주택에 대해 가장 가까운 커피점을 구한다 (거리가 R 이상이면 패스)
 // 2. 모든 주택에 대해 가장 가까운 제과점을 구한다.(거리가 R 이상이면 패스)
 // 3. 주택을 돌면서 커피점과 제과점의 거리합이 작은걸 구한다.
-
+#include <queue>
 #include <vector>
 using namespace std;
 #define MAX_BUILDING 10001
+#define _INF 10e9
 vector<pair<int, int>> adj[MAX_BUILDING]; // 도착지, 거리
 int _N;
 
@@ -35,6 +36,39 @@ void add(int sBuilding, int eBuilding, int mDistance)
 	return;
 }
 
+
 int calculate(int M, int mCoffee[], int P, int mBakery[], int R) {
+
+	// 일반 다익스트라(시작점 - 모든 노드의 최단거리를 구함)
+	vector<int> dist(_N, _INF); // _N 개를 _INF로 초기화
+	priority_queue < pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+	//시작점 초기화
+	int start = 0; 
+	dist[start] = 0;
+	pq.push(make_pair(start, 0)); // 정점, cost
+
+	while (!pq.empty())
+	{
+		pair <int, int> top = pq.top();
+		int now = top.first;
+		int cost = top.second;
+		pq.pop();
+
+		if (dist[now] < cost) continue;
+
+		for (int i = 0; i < adj[now].size(); i++)
+		{
+			int nextnode = adj[now][i].first;
+			int w = adj[now][i].second;
+
+			int nextdist = cost + w;
+			if (nextdist < dist[nextnode])
+			{
+				dist[nextnode] = nextdist;
+				pq.push(make_pair(nextdist, nextnode));
+			}
+		}
+	}
 	return 0;
 }
